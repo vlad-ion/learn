ea21eb0c699f66fc1eacc84125148ef694df4255
vladi <vlad.ion@smi.de>
2016-02-07 12:53:44 +0200

    tuple tuple tuple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-fn main()
+///Prints stuffs "like this"
+///Document like crazy
+pub fn main()
 {
 	println!("Hello Rusted World!");
-	println!("Har har.");
+	println!("Har har {:?} and {} and {1}.", "gigel", "ionel");
+	println!("Custom display {}", TupleThing(5,0));
+}
+
+use std::fmt;
+struct TupleThing(i32,i32);
+impl fmt::Display for TupleThing
+{
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+	{
+		write!(f, "'int32':{},{}", self.0, self.1)
+	}
 }

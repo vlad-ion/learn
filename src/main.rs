c27c6f5b2ccd6b67d18f8d19aaa668f850fd3641
vlad-ion <vlad.ion@gmail.com>
2016-02-07 13:23:04 +0200

    list

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ pub fn main()
 	let compl = Complex{real:3.3, imag:7.2};
 	println!("Display: {}", compl);
 	println!("Debug: {:?}", compl);
+
+	let vv = List(vec![1,10,100]);
+	println!("{}", vv);
+	println!("{:?}", vv);
 }
 
 use std::fmt;
@@ -49,4 +53,21 @@ impl fmt::Display for Complex
 	{
         write!(f, "{} + {}i", self.real, self.imag)
     }
+}
+
+#[derive(Debug)]
+struct List(Vec<i32>);
+impl fmt::Display for List
+{
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+	{
+		let List(ref vec) = *self;
+		try!(write!(f, "["));
+		for(count, v) in vec.iter().enumerate()
+		{
+			if count!= 0 { try!(write!(f, ", ")); }
+			try!(write!(f, "{}", v));
+		}
+		write!(f, "]")
+	}
 }
